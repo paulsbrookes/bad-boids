0244bb82f9f732e862e4b31d93363d23d8d75b20
Paul Brookes <paul.s.brookes@gmail.com>
2016-02-23 19:05:28 +0000

    Created test_boids.test_bad_boids_new_flock_vectorized to test the vectorized new_flock test more fully.

--- test_boids.py
+++ test_boids.py
@@ -25,3 +25,20 @@ def test_bad_boids_new_flock():
             points = new_flock(count, np.array([window[0]]), np.array([window[1]]))
             in_range = (points >= window[0]) * (points <= window[1])
             assert_true(np.all(in_range))
+
+def test_bad_boids_new_flock_vectorized():
+    window_data = yaml.load(
+        open(os.path.join(os.path.dirname(__file__), 'flock_fixtures.yml')))
+    window_limits = np.array(window_data['window_limits'])
+    count_limits = window_data['count_limits']
+    boids_count = np.random.randint(count_limits[0], count_limits[1])
+    vector_limits = window_data['vector_limits']
+    vector_length = np.random.randint(vector_limits[0], vector_limits[1])
+    lower_limits = np.random.uniform(window_limits[0], window_limits[1], vector_length)
+    upper_limits = np.random.uniform(lower_limits, window_limits[1])
+    points = new_flock(boids_count, lower_limits, upper_limits)
+    print points
+    print lower_limits
+    print upper_limits
+    in_range = np.logical_and(points >= lower_limits[:, np.newaxis], points <= upper_limits[:, np.newaxis])
+    assert_true(np.all(in_range))
