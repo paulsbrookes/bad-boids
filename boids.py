0f547955c96198b16ac09b8ce3295a693eadddcd
Paul Brookes <paul.s.brookes@gmail.com>
2016-02-24 12:21:59 +0000

    Vectorize boid repulsion.

--- boids.py
+++ boids.py
@@ -8,8 +8,6 @@ from matplotlib import animation
 import random
 import numpy as np
 
-# Deliberately terrible code for teaching purposes
-
 boid_count = 2
 lower_limits = np.array([-450, 50, 0, -20])
 upper_limits = np.array([100, 1000, 10, 20])
@@ -29,16 +27,22 @@ def update_boids(positions, velocities):
 	repulsion_distance = 10
 	speed_match_distance = 100
 	speed_match_factor = 0.125/len(xs)
+
+	# Move to middle
 	middle = np.mean(positions, 1)
 	direction_to_middle = middle[:, np.newaxis] - positions
 	velocities += direction_to_middle*attraction_strength
 
 	# Fly away from nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			if (xs[j] - xs[i])**2 + (ys[j] - ys[i])**2 < repulsion_distance**2:
-				xvs[i] = xvs[i] + (xs[i] - xs[j])
-				yvs[i] = yvs[i] + (ys[i] - ys[j])
+	displacements = positions[:, np.newaxis, :] - positions[:, :, np.newaxis]
+	squared_displacements = displacements**2
+	square_distances = np.sum(squared_displacements, 0)
+	far_away = square_distances > repulsion_distance**2
+	displacements_if_close = np.copy(displacements)
+	displacements_if_close[0, :, :][far_away] = 0
+	displacements_if_close[1, :, :][far_away] = 0
+	velocities += np.sum(displacements_if_close, 1)
+
 	# Try to match speed with nearby boids
 	for i in range(len(xs)):
 		for j in range(len(xs)):
