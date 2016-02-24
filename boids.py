7f1c3a347dc8f6b235c9b606dd6b6c65df48ea6f
Paul Brookes <paul.s.brookes@gmail.com>
2016-02-24 20:27:38 +0000

    Break up the arguments of Flock into groups that can be more easily supplied when not using default parameters.

--- boids.py
+++ boids.py
@@ -7,8 +7,8 @@ import os
 
 default_params = yaml.load(
         open(os.path.join(os.path.dirname(__file__), 'default_fixture.yml')))
-position_limits = np.array(default_params['position_limits'])
-velocity_limits = np.array(default_params['velocity_limits'])
+def_position_limits = np.array(default_params['position_limits'])
+def_velocity_limits = np.array(default_params['velocity_limits'])
 attraction_strength = default_params['attraction_strength']
 repulsion_distance = default_params['repulsion_distance']
 speed_match_distance = default_params['speed_match_distance']
@@ -17,6 +17,7 @@ boid_count = default_params['boid_count']
 axes_limits = default_params['axes_limits']
 frames = default_params['frames']
 interval = default_params['interval']
+def_movement_params = [attraction_strength, repulsion_distance, speed_match_strength, speed_match_distance]
 
 
 def new_flock(count, lower_limits, upper_limits):
@@ -26,11 +27,13 @@ def new_flock(count, lower_limits, upper_limits):
 
 
 class Flock(object):
-    def __init__(self, default_params=default_params):
-        self.positions = new_flock(
-                            boid_count, position_limits[0], position_limits[1])
-        self.velocities = new_flock(
-                            boid_count, velocity_limits[0], velocity_limits[1])
+    def __init__(self, position_limits=def_position_limits, velocity_limits=def_velocity_limits, boid_count=boid_count, movement_params=def_movement_params):
+        self.positions = new_flock(boid_count, position_limits[0], position_limits[1])
+        self.velocities = new_flock(boid_count, velocity_limits[0], velocity_limits[1])
+        self.attraction_strength = movement_params[0]
+	self.repulsion_distance = movement_params[1]
+	self.speed_match_strength = movement_params[2]
+	self.speed_match_distance = movement_params[3]
 
     def update_boids(self):
         self.accelerate_to_middle()
@@ -41,13 +44,13 @@ class Flock(object):
     def accelerate_to_middle(self):
         middle = np.mean(self.positions, 1)
         direction_to_middle = middle[:, np.newaxis] - self.positions
-        self.velocities += direction_to_middle*attraction_strength
+        self.velocities += direction_to_middle*self.attraction_strength
 
     def accelerate_from_boids(self):
         displacements = self.positions[:, np.newaxis, :] - self.positions[:, :, np.newaxis]
         squared_displacements = displacements ** 2
         self.square_distances = np.sum(squared_displacements, 0)
-        far_away = self.square_distances > repulsion_distance ** 2
+        far_away = self.square_distances > self.repulsion_distance**2
         displacements_if_close = np.copy(displacements)
         displacements_if_close[0, :, :][far_away] = 0
         displacements_if_close[1, :, :][far_away] = 0
@@ -55,11 +58,11 @@ class Flock(object):
 
     def speed_match(self):
         velocity_differences = self.velocities[:, np.newaxis, :] - self.velocities[:, :, np.newaxis]
-        very_far = self.square_distances > speed_match_distance ** 2
+        very_far = self.square_distances > self.speed_match_distance**2
         velocity_differences_if_close = np.copy(velocity_differences)
         velocity_differences_if_close[0, :, :][very_far] = 0
         velocity_differences_if_close[1, :, :][very_far] = 0
-        self.velocities -= (np.mean(velocity_differences_if_close, 1) * speed_match_strength)
+        self.velocities -= (np.mean(velocity_differences_if_close, 1) * self.speed_match_strength)
 
     def move(self):
         self.positions += self.velocities
