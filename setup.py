38a45f78dac2cbe744401a64c4d3fd2b752ee030
Paul Brookes <paul.s.brookes@gmail.com>
2016-02-29 13:17:44 +0000

    Alter setup.py so that yml files are installed.

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     version = "1.0 ",
     packages = find_packages(exclude=['*test']),
     license = "Apache",
+    package_data={
+        '': ['*.yml']
+    },
     author_email = 'paul.s.brookes@gmail.com',
     install_requires = ['numpy', 'matplotlib', 'nose', 'argparse'],
     scripts = ['scripts/boids']
